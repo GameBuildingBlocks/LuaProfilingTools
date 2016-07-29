c91f6471aad44c14ef9ce875f2a2a319242319bc
wansellbusiness <wansellbusiness@hotmail.com>
2016-07-29 10:18:18 +0800

    slua project

---
+++ Assets/hanoi-unity-view/miciluaprofiler/Editor/HanoiUtil.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+public delegate void HanoiNodeAction(HanoiNode n);
+
+public class HanoiUtil 
+{
+    public static void ForeachInParentChain(HanoiNode n, HanoiNodeAction act)
+    {
+        HanoiNode target = n;
+        while (target.Parent != null)
+        {
+            if (act != null)
+                act(target);
+
+            target = target.Parent;            
+        }
+    }
+
+    public static void DrawBlankSpaceRecursively(HanoiNode n)
+    {
+        if (n is HanoiBlankSpace)
+        {
+            Color c = n.GetNodeColor();
+            c.a = 0.5f;
+            n.renderRect = new Rect((float)n.beginTime - DrawingShrinkedAccumulated, 0.0f, HanoiVars.BlankSpaceWidth, HanoiVars.StackHeight * (HanoiVars.DrawnStackCount - 1));
+            HanoiUtil.DrawingShrinkedAccumulated += (float)n.timeConsuming - HanoiVars.BlankSpaceWidth;
+            Handles.DrawSolidRectangleWithOutline(n.renderRect, c, c);
+        }
+
+        if (n.stackLevel == 0)
+        {
+            for (int i = 0; i < n.Children.Count; i++)
+            {
+                DrawBlankSpaceRecursively(n.Children[i]);
+            }
+        }
+    }
+
+    static public float DrawingShrinkedAccumulated = 0.0f;
+    static public float DrawingShrinkedTotal = 0.0f;
+    static public int DrawingCounts = 0;
+    static Dictionary<int, Color> m_colors = new Dictionary<int, Color>();
+
+    public static void DrawRecursively(HanoiNode n)
+    {
+        int hash = n.GetHashCode();
+        Color c;
+        if (!m_colors.TryGetValue(hash, out c))
+        {
+            m_colors[hash] = c = n.GetNodeColor();
+        }
+
+        if (n is HanoiBlankSpace)
+        {
+            HanoiUtil.DrawingShrinkedAccumulated += (float)n.timeConsuming - HanoiVars.BlankSpaceWidth;
+        }
+        else
+        {
+            float renderedWidth = (float)n.timeConsuming;
+
+            n.renderRect = new Rect((float)n.beginTime - DrawingShrinkedAccumulated, HanoiVars.StackHeight * (HanoiVars.DrawnStackCount - n.stackLevel - 1), (float)n.timeConsuming, HanoiVars.StackHeight);
+        }
+
+        Handles.DrawSolidRectangleWithOutline(n.renderRect, c, n.highlighted ? Color.white : c);
+
+        DrawingCounts++;
+
+        for (int i = 0; i < n.Children.Count; i++)
+        {
+            DrawRecursively(n.Children[i]);
+        }
+    }
+
+    public static void DrawLabelsRecursively(HanoiNode n)
+    {
+        if (n.highlighted)
+        {
+            Rect r = n.renderRect;
+
+            r.width = HanoiVars.LabelBackgroundWidth;
+            r.height = 45;
+            Color bg = Color.black;
+            bg.a = 0.5f;
+            Handles.DrawSolidRectangleWithOutline(r, bg, bg);
+
+            GUI.color = Color.white;
+            Handles.Label(new Vector3(n.renderRect.xMin, n.renderRect.yMin), n.funcName);
+            Handles.Label(new Vector3(n.renderRect.xMin, n.renderRect.yMin + 15), n.moduleName);
+            Handles.Label(new Vector3(n.renderRect.xMin, n.renderRect.yMin + 30), string.Format("Time: {0:0.000}", n.timeConsuming));
+        }
+
+        for (int i = 0; i < n.Children.Count; i++)
+        {
+            DrawLabelsRecursively(n.Children[i]);
+        }
+    }
+}
---
+++ Assets/hanoi-unity-view/miciluaprofiler/Editor/VisualizerWindow.cs
@@ -0,0 +1,196 @@
+﻿ using UnityEngine;
+ using UnityEditor;
+ using System.Collections.Generic;
+ 
+     public class VisualizerWindow : EditorWindow
+     {
+         [SerializeField]
+         internal Vector2 m_Scale = new Vector2(1.0f, 1.0f);
+         [SerializeField]
+         internal Vector2 m_Translation = new Vector2(0, 0);
+
+         float m_winWidth = 0.0f;
+         float m_winHeight = 0.0f;
+
+         HanoiData m_data = new HanoiData();
+
+         HanoiNode m_picked;
+
+         [MenuItem("Window/VisualizerWindow")]
+         static void Create()
+         {
+             // Get existing open window or if none, make a new one:
+             VisualizerWindow window = (VisualizerWindow)EditorWindow.GetWindow(typeof(VisualizerWindow));
+             window.Show();
+             window.wantsMouseMove = true;
+         }
+
+         public VisualizerWindow()
+         {
+             m_data.Load("Assets/luaprofiler_jx3pocket.json");
+         }
+
+         public void OnGUI()
+         {
+             CheckForResizing();
+
+             CheckForInput();
+
+             Handles.BeginGUI();
+             Handles.matrix = Matrix4x4.TRS(m_Translation, Quaternion.identity, new Vector3(m_Scale.x, m_Scale.y, 1));
+
+             //Handles.DrawLine(new Vector3(0, 0), new Vector3(300, 300));
+             //Handles.DrawSolidRectangleWithOutline(new Rect(new Vector2(100, 100), new Vector2(50, 50)), Color.green, Color.green);
+             //Debug.LogFormat("time: {0}, window: {1}", Time.time, this.position.ToString());
+
+             DrawHanoiData(m_data.Root);
+
+             Handles.EndGUI();
+         }
+
+         private void CheckForResizing()
+         {
+             if (Mathf.Approximately(position.width, m_winWidth) && 
+                 Mathf.Approximately(position.height, m_winHeight))
+                 return;
+
+             m_winWidth = position.width;
+             m_winHeight = position.height;
+
+             HanoiVars.StackHeight = (m_data.MaxStackLevel != 0) ? (m_winHeight / m_data.MaxStackLevel) : m_winHeight;
+         }
+
+         private void DrawHanoiData(HanoiRoot r)
+         {
+             if (r.callStats == null)
+                 return;
+
+             HanoiVars.LabelBackgroundWidth = GetDrawingLengthByPanelPixels(200);
+             HanoiVars.BlankSpaceWidth = GetDrawingLengthByPanelPixels(50);
+             HanoiVars.DrawnStackCount = m_data.MaxStackLevel;
+
+             HanoiUtil.DrawingCounts = 0;
+
+             // draw 3 passes
+
+             HanoiUtil.DrawingShrinkedTotal = 0;
+             HanoiUtil.DrawingShrinkedAccumulated = 0;
+             HanoiUtil.DrawBlankSpaceRecursively(r.callStats);
+
+             HanoiUtil.DrawingShrinkedTotal = HanoiUtil.DrawingShrinkedAccumulated;
+             HanoiUtil.DrawingShrinkedAccumulated = 0;
+             HanoiUtil.DrawRecursively(r.callStats);
+
+             HanoiUtil.DrawLabelsRecursively(r.callStats);
+         }
+
+         public Vector2 ViewToDrawingTransformPoint(Vector2 lhs)
+         { return new Vector2((lhs.x - m_Translation.x) / m_Scale.x, (lhs.y - m_Translation.y) / m_Scale.y); }
+         public Vector3 ViewToDrawingTransformPoint(Vector3 lhs)
+         { return new Vector3((lhs.x - m_Translation.x) / m_Scale.x, (lhs.y - m_Translation.y) / m_Scale.y, 0); }
+
+         public Vector2 DrawingToViewTransformVector(Vector2 lhs)
+         { return new Vector2(lhs.x * m_Scale.x, lhs.y * m_Scale.y); }
+         public Vector3 DrawingToViewTransformVector(Vector3 lhs)
+         { return new Vector3(lhs.x * m_Scale.x, lhs.y * m_Scale.y, 0); }
+
+         public Vector2 ViewToDrawingTransformVector(Vector2 lhs)
+         { return new Vector2(lhs.x / m_Scale.x, lhs.y / m_Scale.y); }
+         public Vector3 ViewToDrawingTransformVector(Vector3 lhs)
+         { return new Vector3(lhs.x / m_Scale.x, lhs.y / m_Scale.y, 0); }
+
+         public Vector2 mousePositionInDrawing
+         {
+             get { return ViewToDrawingTransformPoint(Event.current.mousePosition); }
+         }
+
+         public float GetDrawingLengthByPanelPixels(int pixels)
+         {
+             return Mathf.Abs(ViewToDrawingTransformPoint(new Vector2(pixels, 0)).x - ViewToDrawingTransformPoint(new Vector2(0, 0)).x); 
+         }
+
+         private void CheckForInput()
+         {
+             switch (Event.current.type)
+             {
+                 case EventType.MouseMove:
+                     {
+                         if (m_picked != null)
+                         {
+                             HanoiUtil.ForeachInParentChain(m_picked, (n) => { n.highlighted = false; });
+                             m_picked = null;
+                         }
+
+                         HanoiNode picked = PickHanoiRecursively(m_data.Root.callStats, mousePositionInDrawing);
+                         if (picked != null)
+                         {
+                             HanoiUtil.ForeachInParentChain(picked, (n) => { n.highlighted = true; });
+                             m_picked = picked;
+
+                             Debug.LogFormat("Picked: f {0}, m {1}", m_picked.funcName, m_picked.moduleName);
+
+                             Repaint();
+                         }
+                         else
+                         {
+                             Debug.LogFormat("Picked nothing.");
+                         }
+                     }
+                     break;
+
+                 case EventType.MouseDrag:
+                     if (Event.current.button == 1)
+                     {
+                         m_Translation.x += Event.current.delta.x;
+                         Repaint();
+                     }
+                     break;
+
+                 case EventType.ScrollWheel:
+                     {
+                         float delta = Event.current.delta.x + Event.current.delta.y;
+                         delta = -delta;
+
+                         // Scale multiplier. Don't allow scale of zero or below!
+                         float scale = Mathf.Max(0.03F, 1 + delta * 0.03F);
+
+                         // Offset to make zoom centered around cursor position
+                         m_Translation.x -= mousePositionInDrawing.x * (scale - 1) * m_Scale.x;
+
+                         // Apply zooming
+                         m_Scale.x *= scale;
+
+                         Repaint();
+                     }
+                     break;
+
+                 default:
+                     break;
+             }
+         }
+
+
+         private HanoiNode PickHanoiRecursively(HanoiNode n, Vector2 mousePos)
+         {
+             if (!n.HasValidRect())
+                 return null;
+
+             if (n.renderRect.xMin > mousePos.x || n.renderRect.xMax < mousePos.x)
+                 return null;
+
+             if (n.renderRect.Contains(mousePos))
+                 return n;
+
+             for (int i = 0; i < n.Children.Count; i++)
+             {
+                 HanoiNode child = PickHanoiRecursively(n.Children[i], mousePos);
+                 if (child != null)
+                 {
+                     return child;
+                 }
+             }
+
+             return null;
+         }
+     }
+ 
